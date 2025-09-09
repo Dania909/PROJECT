5542d7e3f6c6326dd06143783995530748c9a8bc
Dania909 <161945606+Dania909@users.noreply.github.com>
2025-09-09 11:33:49 +0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-900">
+      <h1 className="text-4xl font-bold text-emerald-400">
+        Hello React + Tailwind 
+      </h1>
+    </div>
+  )
+}
